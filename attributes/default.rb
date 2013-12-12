29e8c03b7615d21763600c948e250dd5c43e4ea3
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2013-12-12 15:49:12 +0300

    Use fqdn by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,7 +11,8 @@ default['mongodb']['defaults']['service']['log_file_prefix'] = '/var/log/mongodb
 default['mongodb']['defaults']['service']['config_file_prefix'] = '/etc/mongodb'
 default['mongodb']['defaults']['service']['pid_file_prefix'] = '/var/run'
 # Use fqdn or ip in rs.conf()
-default['mongodb']['defaults']['service']['use_fqdn'] = false
+# For me it was mistake to use ip, because they often changes.
+default['mongodb']['defaults']['service']['use_fqdn'] = true
 
 # Currently, plain text ulimits settings passed to initd script ulimit command
 # May be a security hole if somebody adds custom commands like ';rm -rf /var/log'
