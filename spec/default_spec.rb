b48a637412c962197e540902055b86a0ddf3485d
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-01-28 18:46:02 +0300

    Specs for installed packages

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -9,7 +9,14 @@ describe 'mongodb::default' do
     expect(chef_run).to add_apt_repository('mongodb-10gen')
   end
 
-  it 'installs package mongodb' do
-    expect(chef_run).to install_package('mongodb-10gen')
+  [
+    'mongodb-org-mongos',
+    'mongodb-org-server',
+    'mongodb-org-shell',
+    'mongodb-org-tools'
+  ].each do |pkg|
+    it "installs package #{pkg}" do
+      expect(chef_run).to install_package(pkg)
+    end    
   end
 end
